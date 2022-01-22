37908e64729143c1514743ec6c1786d181f7da5c
ericsayegh <ericsayegh@knights.ucf.edu>
2022-01-22 18:59:01 -0500

    logout stuff fixed

--- mobile_app/lib/home.dart
+++ mobile_app/lib/home.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:mobile_app/main.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -15,7 +17,10 @@ class _HomeState extends State<Home> {
       body: Column(
         children: [
           TextButton(
-              onPressed: (){
+              onPressed: () async {
+                isLoggedIn = false;
+                await storage.deleteAll();
+                Navigator.popUntil(context, ModalRoute.withName('/'));
 
               },
               child: const Text('Logout'),
--- mobile_app/lib/login/intro.dart
+++ mobile_app/lib/login/intro.dart
@@ -12,29 +12,38 @@ class _IntroState extends State<Intro> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/register');
-                },
-                child: const Text('Sign Up'),
-              ),
-              TextButton(
-                onPressed: (){
-                  Navigator.pushNamed(context, '/login');
-                },
-                child: const Text('Login'),
-              ),
-            ],
-          )
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/Capturessss.PNG"),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  child: const Text('Sign Up'),
+                ),
+                TextButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  child: const Text('Login'),
+                ),
+              ],
+            )
 
-        ],
+          ],
 
+        ),
       ),
     );
   }
