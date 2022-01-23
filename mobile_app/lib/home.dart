cb5dc9af77737d1f6f33c6325fb2be58978f7bd5
ericsayegh <ericsayegh@knights.ucf.edu>
2022-01-23 18:18:26 -0500

    blah

--- mobile_app/lib/home.dart
+++ mobile_app/lib/home.dart
@@ -49,15 +49,9 @@ class _HomeState extends State<Home> {
               ),
               IconButton(
                 onPressed: () async {
-                  WidgetsFlutterBinding.ensureInitialized();
 
-                  // Obtain a list of the available cameras on the device.
-                  final cameras = await availableCameras();
 
-                  // Get a specific camera from the list of available cameras.
-                  final firstCamera = cameras.first;
 
-                  
 
                 },
                 icon: const Icon(
