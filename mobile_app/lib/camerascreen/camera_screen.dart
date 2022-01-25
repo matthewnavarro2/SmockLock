cbbd630bdd8a999cba816719dfbaeef43e833a43
ericsayegh <ericsayegh@knights.ucf.edu>
2022-01-24 21:53:17 -0500

    thrr

---
+++ mobile_app/lib/camerascreen/camera_screen.dart
@@ -0,0 +1,78 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/cupertino.dart';
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({Key? key}) : super(key: key);
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController controller;
+  late List cameras;
+  late int selectedCameraIdx;
+  late String imagePath;
+
+  @override
+  void initState() {
+    super.initState();
+    // 1
+    availableCameras().then((availableCameras) {
+
+      cameras = availableCameras;
+      if (cameras.isNotEmpty) {
+        setState(() {
+          // 2
+          selectedCameraIdx = 0;
+        });
+
+        _initCameraController(cameras[selectedCameraIdx]).then((void v) {});
+      }else{
+        print("No camera available");
+      }
+    }).catchError((err) {
+      // 3
+      print('Error: $err.code\nError Message: $err.message');
+    });
+  }
+
+  Future _initCameraController(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+
+    // 3
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+
+    // If the controller is updated then update the UI.
+    // 4
+    controller.addListener(() {
+      // 5
+      if (mounted) {
+        setState(() {});
+      }
+
+      if (controller.value.hasError) {
+        print('Camera error ${controller.value.errorDescription}');
+      }
+    });
+
+    // 6
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
