f374d60b9dca9fa0b14567797bc07451564cbdbe
ericsayegh <ericsayegh@knights.ucf.edu>
2022-03-16 03:13:46 -0400

    initial setup stuff

--- mobile_app/lib/lock_setup/setup2.dart
+++ mobile_app/lib/lock_setup/setup2.dart
@@ -10,18 +10,12 @@ class Setup2 extends StatefulWidget {
 
 class _Setup2State extends State<Setup2> {
 
-  Future<int> wifiStatus() async {
-    int lockStatus = 0;
-
-    while(lockStatus == 0){
-      var res = await Api.checkWifiStatus();
-      //check res to see if status is 0 or 1
-      //set lockstatus to status
-    }
-    // move to next page with assumption it is connected to wifi
-    return 1;
-
-  }
+  bool isFaceRec = false;
+  bool isFaceRec2 = false;
+  bool isFinger = false;
+  bool isFinger2 = false;
+  bool isRFID = false;
+  bool isEKEY = false;
 
   @override
   Widget build(BuildContext context) {
@@ -32,8 +26,89 @@ class _Setup2State extends State<Setup2> {
         body: SingleChildScrollView(
             child: Column(
               children: [
-                Text('Please Connect to the locks access points to connect lock to your wireless network.'),
 
+                const Text("Please choose the modes of authorization "
+                    "that are required for access. "
+                    "Ex.) Choosing Fingerprint and Facial will require "
+                    "both to be verified for the lock to unlock."
+                ),
+
+                ListTile(
+                  title: const Text('Facial Recognition'),
+                  leading: Switch(
+                    value: isFaceRec,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isFaceRec = value;
+                      });
+                    },
+                  ),
+                ),
+
+                ListTile(
+                  title: const Text('Fingerprint Recognition'),
+                  leading: Switch(
+                    value: isFinger,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isFinger = value;
+                      });
+                    },
+                  ),
+                ),
+
+                ListTile(
+                  title: const Text('RFID'),
+                  leading: Switch(
+                    value: isRFID,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isRFID = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Digital E-Key'),
+                  leading: Switch(
+                    value: isEKEY,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isEKEY = value;
+                      });
+                    },
+                  ),
+                ),
+                Text('Would you like to setup any of the following methods of authorizations (These can always be setup later in the settings)?'),
+                ListTile(
+                  title: const Text('Facial Recognition'),
+                  leading: Switch(
+                    value: isFaceRec2,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isFaceRec2 = value;
+                      });
+                    },
+                  ),
+                ),
+                ListTile(
+                  title: const Text('Fingerprint Recognition'),
+                  leading: Switch(
+                    value: isFinger2,
+                    onChanged: (bool value) {
+                      setState(() {
+                        isFinger2 = value;
+                      });
+                    },
+                  ),
+                ),
+
+                TextButton(
+                    onPressed: () {
+
+                    },
+                    child: Text("Next"),
+                )
               ],
             )
         )
