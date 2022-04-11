be3b6e45470897a87ceb7a5b258fdf8bfc538460
ericsayegh <ericsayegh@knights.ucf.edu>
2022-04-10 20:49:10 -0400

    Update finger.dart

--- mobile_app/lib/finger_setup/finger.dart
+++ mobile_app/lib/finger_setup/finger.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:mobile_app/API/api.dart';
 import 'package:mobile_app/utility/authorized_lock_info.dart';
+
+import '../main.dart';
 
 class Finger extends StatefulWidget {
   const Finger({Key? key}) : super(key: key);
@@ -30,9 +33,14 @@ class _FingerState extends State<Finger> {
                 Map<String, dynamic> jsonObject = jsonDecode(res.body);
                 var fingerId = jsonObject['newFpUserId'];
                 // get ip address
-                var masterIP = AuthorizedLocks.masterLock[0]['IP'];
-                var res2 = await Api.startFingerEnrollment(masterIP, fingerId.toString());
-                print(res2);
+                var jwt = await storage.read(key: 'jwt');
+                Map<String, dynamic> decodedToken = JwtDecoder.decode(jwt!);
+                var userId = decodedToken["userId"];
+                var res2 = await Api.getLockUI(userId);
+                Map<String, dynamic> jsonObject2 = jsonDecode(res.body);
+                var ip = jsonObject2["result"][0]["IP"];
+                var res3 = await Api.startFingerEnrollment(ip, fingerId.toString());
+                print(res3);
               },
               child: Text('Start enrollment process'),
           ),
